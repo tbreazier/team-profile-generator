1d78381667f2fa618e1975ffd15ca152e5d5a38a
tbreazier <64601341+tbreazier@users.noreply.github.com>
2020-07-26 13:06:38 -0500

    manager questions complete and working

--- Index.js
+++ Index.js
@@ -7,7 +7,68 @@ const Engineer = require('./lib/Engineer.js');
 const Manager = require('./lib/Manager.js');
 const Intern = require('./lib/Intern.js');
 
+const teamArr = [];
+
 //Add Manager
+const addManager = () => {
+    console.log('Create your team');
+
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: "What is the team manager's name? (Required)",
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter a name!');
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: "What is manager's employee ID? (Required)",
+                validate: idInput => {
+                    if (idInput > 0) {
+                        return true;
+                    } else {
+                        console.log('Please enter valid employee ID!');
+                        return false;
+                    }
+                }
+            }, 
+            {
+                type: 'input',
+                name: 'email',
+                message: "What is manager's email address? (Required)",
+                validate: emailInput => {
+                    if (emailInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter an email address!');
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'officeNumber',
+                message: "What is manager's office number? (Required)",
+                validate: officeNumberInput => {
+                    if (officeNumberInput > 0) {
+                        return true;
+                    } else {
+                        console.log('Please enter a valid office number!');
+                        return false;
+                    }
+                }
+            }
+        ])
+}
 
 //Add Additional Team Members or build page
 
@@ -16,6 +77,6 @@ const Intern = require('./lib/Intern.js');
 //Add Intern
 
 //Start Application
-
+addManager();
 
 
