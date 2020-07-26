631ba2bd750d2e2f98ace0d39b9ba103c4ffc217
tbreazier <64601341+tbreazier@users.noreply.github.com>
2020-07-26 11:25:31 -0500

    employee test created

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,36 @@
+const Employee = require('../lib/Employee.js');
+
+//Test for Employee parameters
+test('New Employee Object Created', () => {
+    const employee = new Employee('Karl', '3255', 'karl@gmail.com')
+
+    expect(employee).toEqual(expect.any(Object)); 
+});
+
+//Test for Employee name
+test('Employee Name', () => {
+    const employee = new Employee('Karl', '3255', 'karl@gmail.com')
+
+    expect(employee.name).toEqual(expect.any(String)); 
+});
+
+//Test for Employee ID
+test('Employee ID', () => {
+    const employee = new Employee('Karl', '3255', 'karl@gmail.com')
+
+    expect(employee.id).toEqual(expect.any(Number)); 
+});
+
+//Test for Employee Email
+test('Employee Email', () => {
+    const employee = new Employee('Karl', '3255', 'karl@gmail.com')
+
+    expect(employee.email).toEqual(expect.any(String)); 
+});
+
+//Test for Employee Role
+test('Employee Role', () => {
+    const employee = new Employee('Karl', '3255', 'karl@gmail.com')
+
+    expect(employee.getRole()).toBe('Employee'); 
+});
