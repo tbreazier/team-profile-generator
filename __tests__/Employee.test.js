5d9de7f41bf5f63b09bcd4bdff72c028b68681a6
tbreazier <64601341+tbreazier@users.noreply.github.com>
2020-07-26 11:35:20 -0500

    initial employee test passed

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -2,35 +2,35 @@ const Employee = require('../lib/Employee.js');
 
 //Test for Employee parameters
 test('New Employee Object Created', () => {
-    const employee = new Employee('Karl', '3255', 'karl@gmail.com')
+    const employee = new Employee('Karl', 3255, 'karl@gmail.com')
 
     expect(employee).toEqual(expect.any(Object)); 
 });
 
 //Test for Employee name
 test('Employee Name', () => {
-    const employee = new Employee('Karl', '3255', 'karl@gmail.com')
+    const employee = new Employee('Karl', 3255, 'karl@gmail.com')
 
     expect(employee.name).toEqual(expect.any(String)); 
 });
 
 //Test for Employee ID
 test('Employee ID', () => {
-    const employee = new Employee('Karl', '3255', 'karl@gmail.com')
+    const employee = new Employee('Karl', 3255, 'karl@gmail.com')
 
     expect(employee.id).toEqual(expect.any(Number)); 
 });
 
 //Test for Employee Email
 test('Employee Email', () => {
-    const employee = new Employee('Karl', '3255', 'karl@gmail.com')
+    const employee = new Employee('Karl', 3255, 'karl@gmail.com')
 
     expect(employee.email).toEqual(expect.any(String)); 
 });
 
 //Test for Employee Role
 test('Employee Role', () => {
-    const employee = new Employee('Karl', '3255', 'karl@gmail.com')
+    const employee = new Employee('Karl', 3255, 'karl@gmail.com')
 
     expect(employee.getRole()).toBe('Employee'); 
 });
